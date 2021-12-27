34b16d0788d8e1c7ba6711d84445aff097296854
Jakub Warmuz <jwarmuz99@gmail.com>
2021-12-27 22:30:13 +0100

    inheritance and payables

--- StorageFactory.sol
+++ StorageFactory.sol
@@ -1,7 +1,25 @@
-pragma solidity ^0.4.0;
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.6.0;
 
-contract StorageFactory {
-    function StorageFactory(){
+import "./SimpleStorage.sol";
 
+contract StorageFactory is SimpleStorage {
+
+    SimpleStorage[] public simpleStorageArray;
+
+    function createSimpleStorageContract() public {
+        SimpleStorage simpleStorage = new SimpleStorage();
+        simpleStorageArray.push(simpleStorage);
     }
+
+    function sfStore(uint256 _simpleStorageIndex, uint256 simpleStorageNumber) public {
+        // Address
+        // ABI
+        SimpleStorage(address(simpleStorageArray[_simpleStorageIndex])).store(simpleStorageNumber);
+    }
+
+    function sfGet(uint256 _simpleStorageIndex) public view returns (uint256) {
+        return SimpleStorage(address(simpleStorageArray[_simpleStorageIndex])).retrieve();
+    }
+
 }
